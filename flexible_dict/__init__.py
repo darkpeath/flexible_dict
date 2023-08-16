41e0c939ec67039843c9a829382b7721724ea281
darkpeath <darkpeath@gmail.com>
2023-08-16 22:48:16 +0800

    chore: generate a _version.py file to save the version info

--- flexible_dict/__init__.py
+++ flexible_dict/__init__.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from .json_object import json_object, Field, MISSING, JsonObject, ObjectVar
-from .version import __version__
+
+try:
+    from ._version import __version__
+except ImportError:
+    try:
+        from setuptools_scm import get_version
+        __version__ = get_version(root='..', relative_to=__file__)
+    except (ImportError, LookupError):
+        __version__ = "UNKNOWN"
 
 __all__ = [
     'json_object', 'Field', 'MISSING', 'JsonObject', 'ObjectVar',
